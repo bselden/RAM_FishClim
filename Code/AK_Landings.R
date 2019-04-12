c65e6dd53cc6528b8690be3a53f854a35b0f3c56
bselden <rebeccaselden@gmail.com>
2019-04-12 15:24:10 -0400

    associate location info for old stat6

--- Code/AK_Landings.R
+++ Code/AK_Landings.R
@@ -15,7 +15,8 @@ ft_spp <- merge(ft, spp, by=c("ADFG_I_SPECIES_CODE", "AKFIN_SPECIES_CODE"))
                               
 
 
-### ADFG Statistical Area shapefiles
+# ADFG Statistical Areas --------------------------------------------------
+### ADFG Statistical Area shapefiles since 2001
 stat6 <- readOGR("GIS_Data/AK_Groundfish_Statistical_Areas_2001/", "Groundfish_Statistical_Areas_2001")
 
 stat6.dat <- as.data.table(stat6@data)
@@ -25,31 +26,62 @@ stat6.dat$lat <- coordinates(stat6)[,2]
 
 plot(lat ~ lon, stat6.dat, xlim=c(-180, -100), cex=0.25)
 
+## historical stat areas (Geodatabase)
+# The input file geodatabase
+fgdb <- "GIS_data/Groundfish_Statistical_Areas_1985-2000.gdb"
 
-# ### Average lat lon from observer data for each stat area (a way to add in some missing stat areas)
-### But there are none that aren't in both
-# obs_stat_loc <- obs[,list(lon=mean(AvgOfRETRIEVAL_LONGITUDE_DD),
-#                            lat=mean(AvgOfRETRIEVAL_LATITUDE_DD)),
-#                      by=list(ADFG_STAT_AREA_CODE)]
-# setorder(obs_stat_loc, ADFG_STAT_AREA_CODE)
+# List all feature classes in a file geodatabase
+subset(ogrDrivers(), grepl("GDB", name))
+fc_list <- ogrListLayers(fgdb)
+print(fc_list)
+
+# Read the feature class
+stat6_old <- readOGR(dsn=fgdb,layer="PVG_Statewide_1986_2000_GCS_WGS1984")
+stat6_old.dat <- as.data.table(stat6_old@data)
+
+stat6_old.dat$lon <- coordinates(stat6_old)[,1]
+stat6_old.dat$lat <- coordinates(stat6_old)[,2]
+
+stat6_old_only <- stat6_old.dat[!(STAT_AREA %in% stat6.dat$STAT_AREA)]
+
+### Combine into one stat_area_loc file
+stat_loc_keep <- c("STAT_AREA", "lon", "lat")
+
+stat6_area_loc <- rbind(stat6.dat[,stat_loc_keep, with=F], stat6_old_only[,stat_loc_keep, with=F])
+plot(lat ~ lon, stat6_area_loc, xlim=c(-180, -100), cex=0.25)
+points(lat ~ lon, stat6_old_only, col="red")
 
 
-
-ft_stat <- merge(ft_spp, stat6.dat, by.x="ADFG_I_STAT_AREA", by.y="STAT_AREA")
+# Merge location information into fish tickets ----------------------------
+ft_stat <- merge(ft_spp, stat6_area_loc, by.x="ADFG_I_STAT_AREA", by.y="STAT_AREA")
 
 ft_nonstat <- ft_spp[, 
                      list(land_all=as.numeric(sum(WHOLE_LBS, na.rm=T)),
-                          land_nonstat=sum(WHOLE_LBS[!(ADFG_I_STAT_AREA %in% stat6.dat$STAT_AREA)])),
-                     by=list(scientificname, AKFIN_YEAR)]
+                          land_nonstat=sum(WHOLE_LBS[!(ADFG_I_STAT_AREA %in% stat6_area_loc$STAT_AREA)])),
+                     by=list(spp, AKFIN_YEAR)]
 ft_nonstat[,"frac.nonstat":=land_nonstat/land_all]
 ### Only a few species where landings in non-stat6 areas represents > 10%
 
-ft_nonstat_area <- ft_spp[!(ADFG_I_STAT_AREA %in% stat6.dat$STAT_AREA),
+ft_nonstat_area <- ft_spp[!(ADFG_I_STAT_AREA %in% stat6_area_loc$STAT_AREA),
                            list(lbs=sum(WHOLE_LBS, na.rm=T)),
                            by=list(ADFG_I_STAT_AREA)]
 setorder(ft_nonstat_area, ADFG_I_STAT_AREA)
 
 #write.csv(ft_nonstat_area, "Data/nonstat_area.csv")
+
+### Species with lots of landings not in stat6 areas
+# Herring (Clupea harengus pallasi)
+# Salmon (Oncorhynchus spp)
+# Shrimp (Panopea generosa, Pandalus borealis, Pandalus hypsinotis, Pandalus platyceros)
+# Urchin (S. franciscanus), sea cucumber (Parastichopus californicus)
+# Crab (Cancer magister)
+ft_nonstat[frac.nonstat>0.5, list(num.years=length(unique(AKFIN_YEAR))), by=list(spp)]
+
+### Most landings in 5 digit STAT_AREAs: FMP_SUBAREA "SEI"
+ft_spp[spp=="Pandalus borealis", list(n_ft=sum(N_FT)), by=list(ADFG_I_STAT_AREA, FMP_SUBAREA)]
+
+### 
+obs_spp[ADFG_STAT_AREA_CODE < 100000]
 
 ### Get geographic information for old statistical areas based on union of new shapefiles
 old_stat <- read.csv("Data/nonstat_area_complete.csv")
